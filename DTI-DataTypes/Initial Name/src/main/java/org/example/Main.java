df252f9f2ab26bb891a974d864b91e9565ab81d5
Muhammad Adi Wicaksono <71773942+adi117@users.noreply.github.com>
2025-02-03 14:55:41 +0700

    Add files via upload

--- DTI-DataTypes/Initial Name/src/main/java/org/example/Main.java
+++ DTI-DataTypes/Initial Name/src/main/java/org/example/Main.java
@@ -1,17 +1,26 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        Scanner inputName = new Scanner(System.in);
+
+        System.out.print("Input your name : ");
+        String name = inputName.nextLine();
+
+        String[] arrayName = name.split(" ");
+
+        ArrayList<String> intialName = new ArrayList<>();
+
+        for (int i = 0; i < arrayName.length; i++) {
+            intialName.add(arrayName[i].split("")[0]);
         }
+
+        System.out.println(String.join("", intialName).toUpperCase());
     }
 }
